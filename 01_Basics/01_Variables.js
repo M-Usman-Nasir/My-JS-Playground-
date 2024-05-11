f118f0f467c67e8d06f45a03c9a0ca6c8c6ae1ef
Usman Nasir <157220338+M-Usman-Nasir@users.noreply.github.com>
2024-05-11 02:11:15 +0000

    const, var & let declarations & commit.

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,15 @@
+const accountId = 8127146
+let accountEmail = "usman@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karachi"
+
+accountEmail = "nasir@gmail.com"
+accountPassword = "678910"
+accountCity = "Lahore"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table ([accountEmail, accountId, accountPassword, accountCity]);
