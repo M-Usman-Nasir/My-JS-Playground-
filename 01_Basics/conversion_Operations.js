f3dc026cf7c9c27947025086589fc3ff245d0620
Usman Nasir <157220338+M-Usman-Nasir@users.noreply.github.com>
2024-05-13 17:42:34 +0000

    conversion of data types & Operations

---
+++ 01_Basics/conversion_Operations.js
@@ -0,0 +1,28 @@
+// let score = 33
+let score = "33" 
+
+// console.log(score);
+
+// console.log (typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number (score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1 or false = 0
+
+let isLogIn = "Usman";
+let booleanIsLogIn = Boolean (isLogIn);
+console.log (booleanIsLogIn);
+
+// 1 = true or 0 = false
+// "" => false
+// "usman" => true
+
+let someNumber = 33;
+let stringNumber = String (someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
