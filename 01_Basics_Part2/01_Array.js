81f08c17eb54446a0d52be4326a5299362ea4683
Usman Nasir <157220338+M-Usman-Nasir@users.noreply.github.com>
2024-05-28 06:37:05 +0000

    Edited

--- 01_Basics_Part2/01_Array.js
+++ 01_Basics_Part2/01_Array.js
@@ -1,10 +1,10 @@
-// array
+// Array
 
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+const myHeors = ["Iqbal", "Jinnah"]
 
 const myArr2 = new Array(1, 2, 3, 4)
-// console.log(myArr[1]);
+console.log(myArr[1], myArr2);
 
 // Array methods
 
@@ -26,14 +26,14 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
 const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
